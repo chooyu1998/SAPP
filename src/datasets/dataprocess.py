52d839d198cd24d0817744652bd78b067b8057e7
chooyu1998 <choooyou98@gmail.com>
2025-05-06 12:02:40 +0900

    Initial commit

---
+++ src/datasets/dataprocess.py
@@ -0,0 +1,63 @@
+import torch
+import numpy as np
+
+aa_dict = {'A': 1,'G': 2,'V': 3,'S': 4,'E': 5,'R': 6,'T': 7,
+'I': 8,'D': 9,'P': 10,'K': 11,'Q': 12,'N': 13,'F': 14,'Y': 15,
+'M': 16,'H': 17,'W': 18,'C': 19,'-': 0,'L': 20}
+
+def torch_binning(data,max_val,steps):
+    bin_edges = torch.linspace(0,max_val,steps=steps)
+    digitized = torch.bucketize(data.unsqueeze(-1)[:,:,-1],bin_edges)
+    one_hot = torch.nn.functional.one_hot(digitized, num_classes=len(bin_edges)+1)
+    
+    return digitized,one_hot
+
+def get_Data(data_list,protein_dic,rsa_dic): #list, dict, dict
+    data_list = []
+    rsa_list = []
+    mask_list =[]
+    rsamask_list = []
+    y_list = []
+
+    for li in tqdm(data_list):
+        protein = li[0]
+        site = li[1]
+        label = li[2]
+        seq = protein_dic[protein]['seq']
+        RSA = rsa_dic[protein]
+        window =25
+        onehot = np.zeros(51)
+        rsa_feat = np.zeros(51)
+        
+        mask = np.zeros(51)
+        rsa_mask = np.zeros(51)
+        start_idx = max(0,site-window)
+        end_idx = min(site+window,len(seq)-1)
+        j = window - (site-start_idx)
+        for i in range(start_idx, end_idx+1):
+            onehot[j] = aa_dict[seq[i]]
+            rsa_feat[j] = RSA[i]
+            if RSA[i] != -1:
+                rsa_mask[j] = 1
+
+            if aa_dict[seq[i]] != 0:
+                mask[j] = 1
+            j += 1
+        
+        data_list.append(onehot)
+        rsa_list.append(rsa_feat)
+        mask_list.append(np.expand_dims(mask,-1)*np.expand_dims(mask,-1).T)
+        rsamask_list.append(np.expand_dims(rsa_mask,-1)*np.expand_dims(rsa_mask,-1).T)
+        y_list.append(label)
+    
+    data_list = np.array(data_list,dtype='int64')
+    data_list = torch.LongTensor(data_list)
+    y_list = torch.tensor(np.array(y_list))
+    rsa_list = torch.tensor(np.array(rsa_list))
+    rsamask_list = torch.BoolTensor(rsamask_list)
+    mask_list = torch.BoolTensor(mask_list)
+    
+    _,rsa_list = torch_binning(rsa_list,1, 20)
+
+    return data_list,rsa_list,mask_list,rsamask_list,y_list
+
